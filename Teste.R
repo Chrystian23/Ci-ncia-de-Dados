14330c1728ebd13858f89b00ef2621f85e1b864d
Chrystian23 <chrystian.jheimes@gmail.com>
2024-12-04 16:53:43 -0400

    Versão 2.0

--- Teste.R
+++ Teste.R
@@ -1,2 +1,23 @@
 #Testando sincronia com o GitHub
-2 + 2 = 
+
+#Instalando pacotes
+install.packages("tidyverse")
+
+#Carregando o pacote
+library(tidyverse)
+
+if(!require(tidyverse)) install.packages("tidyverse")
+
+#Verificando os pacotes instalados
+sessionInfo()
+
+#Exemplo de função no R
+x <- c(1,1,2,3,5,8)
+
+
+y <- c(2,NA)
+mean(y,na.rm=TRUE)
+
+#Calcula a média de x
+mean(x)
+
